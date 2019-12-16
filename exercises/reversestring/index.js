4153de051314de2f15657da8d9ac98854f0b8e9b
Thuong Ho <thuong.t.ho@gmail.com>
2019-12-16 12:34:17 -0800

    add my solution to reverse add 2 other solutions with reduce and for of loop

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// solution 3 with reduce
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// solution 2 without reverse
+// function reverse(str) {
+//   let reversed = '';
+//   // use for of instead of old for loop to avoid errors
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }  
+//   return reversed;
+// }
