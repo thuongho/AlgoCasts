810e39f78fbfba94b7eb39085f00724d02d501c6
Thuong Ho <thuong.t.ho@gmail.com>
2019-12-16 14:33:10 -0800

    add my solutions for palindrome add some notes on alternative solutions with explaination of why it is not the most efficient

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,32 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // let interviewer know you are aware of multiple solutions to the problem
+  // let them know that every is an advance js method to check each item of an array
+  // let them know that this is doing twice as much work and is not the most efficient solution
+  // propose a more direct solution
+  return str.split('').every((char, i) => char === str[str.length - i - 1]);
+}
+
+palindrome('abba');
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   return str === str.split('').reverse().join('');
+// }
+
+// function palindrome(str) {
+//   let reversed = str.split('').reduce((rev, char) => char + rev, '');
+//   debugger;
+//   return str === reversed;
+// }
+
+// function palindrome(str) {
+//   let reversed = '';
+//   for(let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return str === reversed;
+// }
