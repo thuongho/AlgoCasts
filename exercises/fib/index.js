809e44f84a5d03754c527e47a47fee94d8d734a8
Thuong Ho <thuong.t.ho@gmail.com>
2019-12-18 10:12:30 -0800

    update fib with solutions

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,83 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const results = fn.apply(this, args);
+    cache[args] = results;
+    return results;
+  }
+}
+
+// O(2^n) - exponential
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
+
+// // O(n) - linear 
+// // for every increase in n we have to calc one number additional number
+// function fib(n) {
+//   const arr = [];
+//   // i++ is the additional number
+//   // alt: we have a for loop that will iterate over a close set, 1:1
+//   // starting out with a fix number, always incrementing by one up to a fix target
+//   for (let i = 0; i <= n; i++) {
+//     if (i < 2) {
+//       arr.push(i);
+//     } else {
+//       arr.push(arr[i - 1] + arr[i - 2]);
+//     }
+//   }
+//   return arr[arr.length - 1];
+// }
+
+// function fib(n, results = [0, 1]) {
+//   if (results.length - 1 >= n) {
+//     return results[n];
+//   }
+
+//   const add = results[results.length - 1] + results[results.length - 2];
+//   results.push(add);
+//   return fib(n, results);
+// }
+
+// O(2^n) - exponential
+// for every increase in n causes dramatic increase in time
+// hey this looks really slow, how can we speed it up? memoization
+// save the ran function (fib(4)) into memory and return that result from memory everytime the same function is called (fib(4))
+// function memoize(fn) {
+//   const cache = {};
+//   return function(...args) {
+//     if (cache[args]) {
+//       return cache[args];
+//     }
+
+//     // run the fib sequence and save it into cache
+//     const result = fn.apply(this, args);
+//     cache[args] = result;
+//     return result;
+//   }
+// }
+
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+//   return fib(n - 1) + fib(n - 2);
+// }
+
+// // super charge the fib
+// fib = memoize(fib);
