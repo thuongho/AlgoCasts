7e712cc13c7e4fa89c3d3f02e0b314df2a9f39d8
Thuong Ho <thuong.t.ho@gmail.com>
2019-12-18 12:56:27 -0800

    add solution to building queue from stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,40 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stackOne = new Stack();
+    this.stackTwo = new Stack();
+  }
+
+  add(item) {
+    this.stackOne.push(item);
+  }
+
+  remove() {
+    while (this.stackOne.peek()) {
+      this.stackTwo.push(this.stackOne.pop());
+    }
+    const removeItem = this.stackTwo.pop();
+    // restore original stack
+    while (this.stackTwo.peek()) {
+      this.stackOne.push(this.stackTwo.pop());
+    }
+    return removeItem;
+  }
+
+  peek() {
+    while (this.stackOne.peek()) {
+      this.stackTwo.push(this.stackOne.pop());
+    }
+
+    const peekItem = this.stackTwo.peek();
+
+    while (this.stackTwo.peek()) {
+      this.stackOne.push(this.stackTwo.pop());
+    }
+    return peekItem;
+  }
+}
 
 module.exports = Queue;
