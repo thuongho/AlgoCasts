5826c794825e23341855a945178d3a0010625662
Thuong Ho <thuong.t.ho@gmail.com>
2019-12-18 19:47:03 -0800

    add levelwidth solution

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  // width means BF - add children to the end
+  const counters = [0];
+  const nodes = [root, 's'];
+
+  while (nodes.length > 1) {
+    const node = nodes.shift();
+    if (node === 's') {
+      counters.push(0);
+      nodes.push('s');
+    } else {
+      nodes.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+}
 
 module.exports = levelWidth;
